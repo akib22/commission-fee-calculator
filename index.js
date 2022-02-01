a77db1f4acaa775f3f4aa705442ea1631f5e7f06
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2022-02-02 00:49:20 +0600

    # feature  - command file argument file data reading complete

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 
+const {readFile} = require('fs');
+const {resolve} = require('path');
 const {CaseIn, CashOutJuridical, CashOutNatural} = require('./services');
 
-Promise.all([
-  new CaseIn().get(),
-  new CashOutJuridical().get(),
-  new CashOutNatural().get(),
-]).then(result => {
-  console.log(result);
+readFile(resolve(process.argv[2]), {encoding: 'utf8'}, (err, transactions) => {
+  if (err) {
+    process.stderr.write('input file error\n');
+    process.exit(1);
+  }
+
+  const parsedTransactions = JSON.parse(transactions);
+
+  // fetch commission fee's configurations
+  Promise.all([
+    new CaseIn().get(),
+    new CashOutJuridical().get(),
+    new CashOutNatural().get(),
+  ]).then(result => {
+    console.log(parsedTransactions);
+    console.log(result);
+  });
 });
