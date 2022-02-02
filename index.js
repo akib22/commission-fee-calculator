1b29fc7602a72fb49e9ca8006d5845a89b41714b
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2022-02-02 11:07:27 +0600

    # improvement  - if json format is not correct program will exit

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ require('dotenv').config();
 
 const {readFile} = require('fs');
 const {resolve} = require('path');
+const {jsonParser} = require('./utils/jsonParser');
 const {CaseIn, CashOutJuridical, CashOutNatural} = require('./services');
 
-readFile(resolve(process.argv[2]), {encoding: 'utf8'}, (err, transactions) => {
-  if (err) {
-    process.stderr.write('input file error\n');
-    process.exit(1);
+readFile(
+  resolve(process.argv[2] || ''),
+  {encoding: 'utf8'},
+  (err, transactions) => {
+    if (err) {
+      process.stderr.write('input file error\n');
+      process.exit(1);
+    }
+
+    const parsedTransactions = jsonParser(transactions);
+
+    if (!parsedTransactions) {
+      process.stderr.write('json format not correct\n');
+      process.exit(1);
+    }
+
+    // fetch commission fee's configurations
+    Promise.all([
+      new CaseIn().get(),
+      new CashOutJuridical().get(),
+      new CashOutNatural().get(),
+    ]).then(result => {
+      console.log(parsedTransactions);
+      console.log(result);
+    });
   }
-
-  const parsedTransactions = JSON.parse(transactions);
-
-  // fetch commission fee's configurations
-  Promise.all([
-    new CaseIn().get(),
-    new CashOutJuridical().get(),
-    new CashOutNatural().get(),
-  ]).then(result => {
-    console.log(parsedTransactions);
-    console.log(result);
-  });
-});
+);
